5d7fdeff88450e1e72cac3678a931cc76694106b
zerry101 <macone394@gmail.com>
2023-07-13 23:00:55 +0530

    added props concept

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   const name = "john";
   const age = <h1>45</h1>
 
   return (
-    <div className="App">
+    <div className={styles.App}>
       Hello {name}of age {age}
       <div>
-        {GetNameComponent()}
+        <GetNameComponent/>
       </div>
       <div>
       </div>
-      <SubApp firstname="john1" personage={45} email="xyz@gmail.com"/>
+      <SubApp  firstname="john1" personage={45} email="xyz@gmail.com"/>
       <SubApp firstname="john2" personage={46} email="abc@gmail.com"/>
       <SubApp firstname="john3" personage={47} email="pqr@gmail.com"/>
-      <Job salary={90000} position="Senior SDE" company="Amazon"></Job>
+      <Job  salary={90000} position="Senior SDE" company="Amazon"></Job>
       <Job salary={80000} position="Junior SDE" company="Microsoft"></Job>
     </div>
   );
 }
 
+
+// props is an object of which arguuments are to be passed from component jsx element  
 
 const Job = (props)=>{
   return (
@@ -35,7 +37,7 @@ const Job = (props)=>{
 const SubApp = (props) => {
 
   return (
-    <div>
+    <div className={styles.name}>
       <h1>{props.firstname}</h1>
       <h1>{props.personage}</h1>
       <h1>{props.email}</h1>
