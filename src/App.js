c506c048fecf154dd7bf1ba91956cd90394ac765
zerry101 <macone394@gmail.com>
2023-07-14 09:05:31 +0530

    added props concept

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ function App() {
   const name = "john";
   const age = <h1>45</h1>
 
+  const userAge = 18;
+  const isGreen = false;
   return (
     <div className={styles.App}>
       Hello {name}of age {age}
+      {userAge >= 18 ? <h1>User has age either bove 18 or 18</h1> : <h1>User has underage</h1>}
+      <h1 style={{ color: isGreen ? "green" : "purple" }}>This has purple color</h1>
       <div>
-        <GetNameComponent/>
+        <GetNameComponent />
       </div>
       <div>
       </div>
-      <SubApp  firstname="john1" personage={45} email="xyz@gmail.com"/>
-      <SubApp firstname="john2" personage={46} email="abc@gmail.com"/>
-      <SubApp firstname="john3" personage={47} email="pqr@gmail.com"/>
-      <Job  salary={90000} position="Senior SDE" company="Amazon"></Job>
+      <SubApp firstname="john1" personage={45} email="xyz@gmail.com" />
+      <SubApp firstname="john2" personage={46} email="abc@gmail.com" />
+      <SubApp firstname="john3" personage={47} email="pqr@gmail.com" />
+      <Job salary={90000} position="Senior SDE" company="Amazon"></Job>
       <Job salary={80000} position="Junior SDE" company="Microsoft"></Job>
     </div>
   );
@@ -24,7 +28,7 @@ function App() {
 
 // props is an object of which arguuments are to be passed from component jsx element  
 
-const Job = (props)=>{
+const Job = (props) => {
   return (
     <div>
       <h1>{props.salary}</h1>
