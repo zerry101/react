f2887b949da2e7b656b78c0a09b24b5e2f4d2cd5
zerry101 <macone394@gmail.com>
2023-07-14 19:36:23 +0530

    added props concept

--- src/Todo.js
+++ src/Todo.js
@@ -6,7 +6,7 @@ export function Todo() {
     const [newTask, setNewTask] = useState("");
     const [todoList, setTodoList] = useState(["fsfsfs","afdsfdsfsd"]);
 
-    const handleNewTask = (event) => {
+    const hanldleNewtask = (event) => {
         setNewTask(event.target.value)
     }
 
@@ -15,13 +15,25 @@ export function Todo() {
         const newTodoList=[ ...todoList, newTask];
         setTodoList([...todoList,newTask]);
     }
+
+
+    const deleteTask=(task)=>{
+        console.log('====================================');
+        console.log(todoList);
+        console.log('====================================');
+        const deletedList=todoList.splice(todoList.indexOf(task),1);
+        console.log('====================================');
+        console.log(deletedList);
+        console.log('====================================');
+    }
+
     return (
         <div>
-            <input onChange={handleNewTask} />
+            <input onChange={hanldleNewtask} />
             <button onClick={pushValueToList}>Add This task</button>
 
-            {todoList.map((ele, index) => {
-                return <h1>{ele}</h1>;
+            {todoList.map((task, index) => {
+                return <div><h1>{task}</h1><button onClick={(task)=>{deleteTask(task)}}></button></div>;
             })}
         </div>
     );
